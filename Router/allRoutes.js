5c630921329b822d631a77891227bd78153235ea
Bioplant01 <carreras.chris@yahoo.fr>
2025-07-11 21:04:30 +0200

    Add edit password page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,8 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/Pages/home.html"),
-    new Route("/signin", "Connexion", "/Pages/signin.html"),
-    new Route("/signup", "Inscription", "/Pages/signup.html"),
+    new Route("/signin", "Connexion", "/Pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/Pages/auth/signup.html"),
+    new Route("/account", "compte", "/Pages/account.html"),
+    new Route("/carpooling", "Covoiturage", "/Pages/carpooling.html"),
+    new Route("/editPassword", "Modifier mot de passe", "/Pages/auth/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
